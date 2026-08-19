10e42cc5e56d42fc9d8e96bdd7fc26375594e2ff
Brenda Vogel <breenvogel07@gmail.com>
2026-08-19 09:20:03 -0300

    added enviroment devenv and updated the analytical function by linear transient 1d

--- src/core/solvers/analytical/linear_transient_1d.py
+++ src/core/solvers/analytical/linear_transient_1d.py
@@ -1,16 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-#parameters
-Pw=120
-Pe=100
-L=1000
-k=0.3
-theta=0.2
-ct=0.000001
-u=2
-t_final=1000
-n_modos=100
 
 def linear_trans_1d(Pe,Pw,L,k,theta,ct,u,t_final,n_modos):
 
@@ -29,44 +17,3 @@ def linear_trans_1d(Pe,Pw,L,k,theta,ct,u,t_final,n_modos):
     P=(Pe-Pw)*((X/L)+(2/np.pi)*serie)+Pw
 
     return x,t,X,T,P
-
-x,t,X,T,P=linear_trans_1d(
-    Pe,
-    Pw,
-    L,
-    k,
-    theta,
-    ct,
-    u,
-    t_final,
-    n_modos
-)
-# P em função de x
-t_escolhido=500
-
-indice_t=np.argmin(np.abs(t-t_escolhido))
-
-plt.plot(x,P[indice_t,:])
-
-plt.xlabel('x')
-plt.ylabel('P')
-plt.title(f'Pressão em função de x - t={t[indice_t]:.2f}')
-
-plt.grid()
-
-plt.show()
-
-# P em função de t
-x_escolhido=500
-
-indice_x=np.argmin(np.abs(x-x_escolhido))
-
-plt.plot(t,P[:,indice_x])
-
-plt.xlabel('t')
-plt.ylabel('P')
-plt.title(f'Pressão em função de t - x={x[indice_x]:.2f}')
-
-plt.grid()
-
-plt.show()
