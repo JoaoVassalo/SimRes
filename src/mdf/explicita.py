1e46984cdb22745f9da3b313ee8053073c2694f4
Brenda Vogel <breenvogel07@gmail.com>
2026-09-10 16:02:06 -0300

    adicionei mais um tipo de contorno

--- src/mdf/explicita.py
+++ src/mdf/explicita.py
@@ -78,3 +78,76 @@ def explicita_1d_FP(
         P_old = P_new.copy()
 
     return x, t, P, lambd
+
+def explicita_1d_PP(
+    p0,
+    Pw,
+    h,
+    L,
+    largura,
+    k,
+    phi,
+    ct,
+    mu,
+    t_final,
+    nx,
+    nt,
+):
+
+    A = h * largura
+
+    # malha espacial
+    dx = L / nx
+    x = np.linspace(dx / 2, L - dx / 2, nx)
+
+    # coeficiente de difusividade hidraulica
+    eta = k / (phi * ct * mu)
+
+    # malha temporal
+    dt = t_final / nt
+    t = np.linspace(0, t_final, nt + 1)
+
+    rx = dt / dx**2
+    lambd = eta * rx
+
+    # condição de estabilidade
+    if lambd > 0.5:
+        print("A formulação explícita não satisfaz a condição de estabilidade.")
+    else:
+        print("A condição de estabilidade foi satisfeita.")
+
+    # condição inicial
+    P_old = np.ones(nx) * p0
+
+    # armazenamento
+    P = np.zeros((nt + 1, nx))
+    P[0, :] = P_old
+
+    # loop temporal
+    for n in range(nt):
+
+        P_new = np.zeros(nx)
+
+        # fronteira esquerda - fluxo prescrito
+        P_new[0] = ((1 - 4*lambd) * P_old[0] +(4/3)*lambd * P_old[1]
+        +(8/3)*lambd *Pw)
+        # nós internos - diferença centrada
+        for i in range(1, nx - 1):
+
+            P_new[i] = (
+                lambd * P_old[i - 1]
+                + (1 - 2 * lambd) * P_old[i]
+                + lambd * P_old[i + 1]
+            )
+
+        # fronteira direita - pressão prescrita
+        P_new[-1] = (
+            (1 - 4*lambd) * P_old[-1]
+            + (4/3)*lambd * P_old[-2]
+            +(8/3)*lambd *p0)
+
+        P[n + 1, :] = P_new
+
+        P_old = P_new.copy()
+
+    return x, t, P, lambd
